55d2524a130b4028afb3023e846a4daab627cd2a
Jaedan Parsons <jaedanparsons@gmail.com>
2020-08-03 14:00:21 -0700

    Completed Bottom Button Bar
    Notes:
    - searchable.xml is unimportant in this version and can be deleted since it doesn't do anything as of yet
    - all the buttons link to fragments which in this update have only colors and their names
    
    - if errors pop up saying "Cannot resolve symbol 'Test' / 'jupiter'" it doesn't do anything to the code and you can ignore those errors as long as the code runs
    
    - Also there are some warning say that the files should have @strings instead of being hard coded, this is a problem an should be changed but was ok in this current version.

--- app/src/main/java/com/example/mobileappgit/MainActivity.java
+++ app/src/main/java/com/example/mobileappgit/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mobileappgit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener); // 11:30
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_plus:
+                            selectedFragment = new NewTradeFragment();
+                            break;
+                        case R.id.nav_communicate:
+                            selectedFragment = new CommunicateFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
