3684a19c90d5f5c605a5a7e460e73d2a44cf5464
Jaedan Parsons <jaedanparsons@gmail.com>
2020-08-19 12:38:04 -0700

    Completed Bottom Button Bar
    Notes:
    - searchable.xml is unimportant in this version and can be deleted since it doesn't do anything as of yet
    - all the buttons link to fragments which in this update have only colors and their names
    
    - if errors pop up saying "Cannot resolve symbol 'Test' / 'jupiter'" it doesn't do anything to the code and you can ignore those errors as long as the code runs
    
    - Also there are some warning say that the files should have @strings instead of being hard coded, this is a problem an should be changed but was ok in this current version.
    
    Notes for Jaedan's_JacobsBottomButtonBar
    Current push has errors
    
    SharedPreferences Works!!
    problem is currently there is no way to turn it off and you can't sign out
    
    Email doesn't work yet because of error that I emailed the teacher about, lol
    
    The app doesn't crash automaticly but it does not work yet

--- app/src/main/java/com/example/mobileappgit/CommunicateFragment.java
+++ app/src/main/java/com/example/mobileappgit/CommunicateFragment.java
@@ -1,9 +1,12 @@
 package com.example.mobileappgit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,9 +18,50 @@ import androidx.fragment.app.Fragment;
  * as well as send messages/offers through the app
  */
 public class CommunicateFragment extends Fragment {
+    private EditText mEditTextTo;
+    private EditText mEditTextSubject;
+    private EditText mEditTextMessage;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_communicate, container,false);
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+/*        setContentView(R.layout.email_message_make);*/ // already done
+
+/*        mEditTextTo = mEditTextTo.findViewById(R.id.edit_text_message);
+        mEditTextSubject = mEditTextSubject.findViewById(R.id.edit_text_subject);
+        mEditTextMessage = mEditTextMessage.findViewById(R.id.edit_text_message);
+        Button buttonSend = buttonSend.findViewById(R.id.button_send);*/
+        mEditTextTo = findViewById(R.id.edit_text_message);
+        mEditTextSubject = findViewById(R.id.edit_text_subject);
+        mEditTextMessage = findViewById(R.id.edit_text_message);
+        Button buttonSend = findViewById(R.id.button_send);
+        Button buttonSend = view.findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail() {
+        String recipientList = mEditTextTo.getText().toString();
+        String[] recipients = recipientList.split(",");
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
+    }
+
 }
